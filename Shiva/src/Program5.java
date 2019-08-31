5f9ab573f9e3d76057dfb22bc9b068fd016fe47a
Muthyapu15 <mani15491@gmail.com>
2019-08-31 11:30:27 +0530

    first pull

---
+++ Shiva/src/Program5.java
@@ -0,0 +1,13 @@
+
+public class Program5 {
+
+	public static void main(String[] args) {
+		 System.out.println("main method started");
+		 boolean Result1 =true;
+		 System.out.println(Result1);
+		 boolean Result2= false;
+		 System.out.println(Result2);
+         System.out.println("main method ended");
+	}
+
+}
---
+++ Shiva/src/Program9.java
@@ -0,0 +1,14 @@
+
+public class Program9 {
+	int add() {
+		int a=10;
+		int b=20;
+		int c=a+b;
+		return c;
+	}
+public static void main(String args[]) {
+	Program9 ad = new Program9();
+ int result = ad.add();
+ System.out.println(result);
+}
+}
