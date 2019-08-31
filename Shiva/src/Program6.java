3c4953825914e847fafa39776b0c84e2d0e0b4aa
Muthyapu15 <mani15491@gmail.com>
2019-08-31 11:29:43 +0530

    First OULL

---
+++ Shiva/src/Program6.java
@@ -0,0 +1,19 @@
+// Reintization of a variable
+public class Program6 {
+
+	public static void main(String[] args) {
+		System.out.println("main method ended");
+		int a=34;
+		System.out.println(a);
+		// re initization of a variable
+		 a=678;
+		System.out.println(a);
+		a=32433;
+		System.out.println(a);
+		System.out.println("main method ended ");
+
+	}
+
+}
+//any variable declare with FINAL keyword cannot be reinitization
+//FINAl int a =34;
---
+++ Shiva/src/Program7.java
@@ -0,0 +1,16 @@
+// Collection of datatypes
+public class Program7 {
+
+	public static void main(String[] args) {
+		System.out.println("main method started");
+		// declaration of a variable 
+		String s1;
+		// intization of a variable
+		s1 = "java@1356";
+		// utilization of a variable
+		System.out.println(s1);
+		System.out.println("main method ended");
+
+	}
+
+}
